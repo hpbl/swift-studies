69947084fe2daebd510deb3faa866d720cae7270
Hilton Pintor <hiltonpintor@gmail.com>
2019-08-31 17:18:50 -0300

    Adding day 02

--- daily-coding-problem.playground/Contents.swift
+++ daily-coding-problem.playground/Contents.swift
@@ -52,3 +52,51 @@ extension Array where Element == Int {
 
 AddToKTestCase.defaultTestSuite.run()
 
+
+/*
+ DAY 02:
+ 
+ This problem was asked by Uber.
+ 
+ Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+ 
+ For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+ 
+ Follow-up: what if you can't use division?
+*/
+
+class ExludingProductTestCase: XCTestCase {
+    func testExcludingProduct_1() {
+        XCTAssertEqual([1, 2, 3, 4, 5].excludingProduct(), [120, 60, 40, 30, 24])
+    }
+    
+    func testExcludingProduct_2() {
+        XCTAssertEqual([3, 2, 1].excludingProduct(), [2, 3, 6])
+    }
+}
+
+extension Array where Element == Int {
+    func division_excludingProduct() -> Array<Int> {
+        let arrayProduct = self.reduce(1, *)
+        return self.map { element in arrayProduct / element}
+    }
+    
+    func excludingProduct() -> Array<Int> {
+        // Construct a temporary array left[] such that left[i] contains product of all elements on left of arr[i] excluding arr[i].
+        var left = Array<Int>(repeating: 1, count: self.count)
+        for index in 1..<self.count {
+            left[index] = self[index - 1] * left[index - 1]
+        }
+        
+        // Construct another temporary array right[] such that right[i] contains product of all elements on on right of arr[i] excluding arr[i].
+        var right = Array<Int>(repeating: 1, count: self.count)
+        for index in stride(from: self.count - 2, through: 0, by: -1) {
+            right[index] = self[index + 1] * right[index + 1]
+        }
+        
+        // To get prod[], multiply left[] and right[].
+        return zip(left, right).map(*)
+    }
+}
+
+ExludingProductTestCase.defaultTestSuite.run()
