99967ee9ef1c95ba28676fe099b310fe6a62840a
Hilton Pintor <hiltonpintor@gmail.com>
2019-09-10 10:40:05 -0300

    Adding day 09

--- daily-coding-problem.playground/Contents.swift
+++ daily-coding-problem.playground/Contents.swift
@@ -350,7 +350,6 @@ PossibleDecodingTestCase.defaultTestSuite.run()
     1   1
 */
 
-
 class UnivalTreeTestCase: XCTestCase {
     func testCountUnivalSubtreesLeaf() {
         let root = Node(val: "1")
@@ -463,3 +462,16 @@ extension Node {
 }
 
 UnivalTreeTestCase.defaultTestSuite.run()
+
+
+/*
+ Day 09:
+ 
+ This problem was asked by Airbnb.
+ 
+ Given a list of integers, write a function that returns the largest sum of non-adjacent numbers. Numbers can be 0 or negative.
+ 
+ For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5. [5, 1, 1, 5] should return 10, since we pick 5 and 5.
+ 
+ Follow-up: Can you do this in O(N) time and constant space?
+*/
